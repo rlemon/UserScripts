b4b5ae42f387285f19181528f5c78d2b7a11cc40
rlemon <rob.lemon@gmail.com>
2011-12-12 18:48:12 -0500

    fixed typo bug

--- Backtrack/backtrack.user.js
+++ Backtrack/backtrack.user.js
@@ -3,44 +3,44 @@
 // @author        rlemon
 // @version       0.3
 // @namespace     rlemon.com
-// @description	  Gives Windows functionality to the Browser Backspace button.
+// @description   Gives Windows functionality to the Browser Backspace button.
 // @include       *
 // ==/UserScript==
 
 function EmbedCodeOnPage(kode) {
-	var script = document.createElement('script');
-	script.textContent = kode;
-	document.head.appendChild(script);
+    var script = document.createElement('script');
+    script.textContent = kode;
+    document.head.appendChild(script);
 }
 function EmbedFunctionOnPageAndExecute(fn) {
-	EmbedCodeOnPage("(function() {" + fn.toString() + "})();")
-};
+    EmbedCodeOnPage("(function() {" + fn.toString() + "})();");
+}
 
 EmbedFunctionOnPageAndExecute(function() {
-	var BackTrackListener = function(event) {
-		var target = event.target;
-		if( isValidEvent(event) && isValidTarget(target) ) {
-			window.setTimeout(function() {
-				BackTrack(event.shiftKey);
-			}, 0);
-			return false;
-		}
-		return true;
-	};
-	var invalidTypes = {
-		text : true,
-		textarea : true,
-		password : true
-	};
-	function BackTrack(forward) {
-		var method = forward ? 'forward' : 'back';
-		window.history[method]();
-	}
-	function isValidTarget(target) {
-		return target && !( target.type in invalidTypes || target.isContentEditable );
-	}
-	function isValidEvent(event) {
-		return !( event.ctrlKey || event.shiftKey ) && event.which === 8;
-	}
-	document.onkeydown = BackTrackListener;
+    var BackTrackListener = function(event) {
+        var target = event.target;
+        if( isValidEvent(event) && isValidTarget(target) ) {
+            window.setTimeout(function() {
+                BackTrack(event.shiftKey);
+            }, 0);
+            return false;
+        }
+        return true;
+    };
+    var invalidTypes = {
+        text : true,
+        textarea : true,
+        password : true
+    };
+    function BackTrack(forward) {
+        var method = forward ? 'forward' : 'back';
+        window.history[method]();
+    }
+    function isValidTarget(target) {
+        return target && !( target.type in invalidTypes || target.isContentEditable );
+    }
+    function isValidEvent(event) {
+        return !( event.ctrlKey || event.shiftKey ) && event.which === 8;
+    }
+    document.onkeydown = BackTrackListener;
 });
